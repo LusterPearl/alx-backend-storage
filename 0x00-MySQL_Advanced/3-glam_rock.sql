47bf8ea1d4cefff90d42215f24f728a787a3faee
LusterPearl <Salamioluwatosin90@gmail.com>
2024-04-25 21:52:03 +0100

    task3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,9 @@
--- List bands with Glam rock as their main style, ranked by longevity
-SELECT band_name, 
-    IFNULL(SUBSTRING_INDEX(lifespan, '-', 1), 2022) - SUBSTRING_INDEX(lifespan, '-', -1) as lifespan
-FROM bands
+-- List bands with Glam rock as main style, ranked by longevity
+SELECT band_name,
+       CASE
+           WHEN split > formed THEN split - formed
+           ELSE 2022 - formed
+       END as lifespan
+FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
