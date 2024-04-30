da5697f4af0f6bb21da9d32dd2ba8c09acecd957
LusterPearl <Salamioluwatosin90@gmail.com>
2024-04-30 15:40:56 +0100

    reddis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+"""
+Module for Cache class to interact with Redis
+"""
+
+
+import redis
+import uuid
+from typing import Callable, Union
+from functools import wraps
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    @staticmethod
+    def count_calls(method: Callable) -> Callable:
+        """
+        Store data in Redis using a randomly generated key
+
+        Args:
+            data (Union[str, bytes, int, float]): The data to store
+
+        Returns:
+            str: The key under which the data is stored
+        """
+        @wraps(method)
+        def wrapper(self, *args, **kwargs):
+            key = method.__qualname__
+            self._redis.incr(key)
+            return method(self, *args, **kwargs)
+        return wrapper
+
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Union string, int , float"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+    @staticmethod
+    def call_history(method: Callable) -> Callable:
+        """Callable list"""
+        @wraps(method)
+        def wrapper(self, *args, **kwargs):
+            inputs_key = "{}:inputs".format(method.__qualname__)
+            outputs_key = "{}:outputs".format(method.__qualname__)
+
+            self._redis.rpush(inputs_key, str(args))
+            output = method(self, *args, **kwargs)
+            self._redis.rpush(outputs_key, output)
+
+            return output
+        return wrapper
+
+    @classmethod
+    def replay(cls, method: Callable) -> None:
+        """Callable list"""
+        inputs_key = "{}:inputs".format(method.__qualname__)
+        outputs_key = "{}:outputs".format(method.__qualname__)
+
+        inputs = cls._redis.lrange(inputs_key, 0, -1)
+        outputs = cls._redis.lrange(outputs_key, 0, -1)
+
+        print("{} was called {} times:".format(method.__qualname__, len(inputs)))
+
+        for inp, out in zip(inputs, outputs):
+            print("{}(*{}) -> {}".format(method.__qualname__, inp.decode(), out.decode()))
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """SELF, key, float, string"""
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """data (Union[str, bytes, int, float])"""
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """data (Union[str, bytes, int, float])"""
+        return self.get(key, fn=lambda d: int(d))
---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Module for implementing an expiring web cache and trackers
+using Redis.
+"""
+
+import requests
+import redis
+import time
+
+
+"""Connect to Redis"""
+redis_client = redis.Redis()
+
+def get_page(url: str) -> str:
+    """
+    Fetches the HTML content of a given URL and caches it with an
+    expiration time of 10 seconds.
+    
+    Args:
+        url (str): The URL to fetch the content from.
+    
+    Returns:
+        str: The HTML content of the URL.
+    """
+    url_count_key = f"count:{url}"
+    redis_client.incr(url_count_key)
+
+    """Cache the result with an expiration time of 10 seconds"""
+    cache_key = f"cache:{url}"
+    cached_content = redis_client.get(cache_key)
+    if cached_content:
+        return cached_content.decode()
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        content = response.text
+        redis_client.setex(cache_key, 10, content)
+        return content
+    else:
+        return f"Failed to fetch URL: {url}"
